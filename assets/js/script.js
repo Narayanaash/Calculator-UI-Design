feddfaeef93bde40c3509916de55c805cd10374d
aash narayan mahato <ankm786@gmail.com>
2021-01-03 14:08:51 +0530

    added vibration for phone

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.buttons > div').bind('touchstart mousedown',function(){
         $(this).addClass('active')
+        window.navigator.vibrate(200); 
     }).bind('mouseup mouseleave touchend', function(){
         $(this).removeClass('active')
     });
